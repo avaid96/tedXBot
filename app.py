c614f489b6929def2716844e3a9d93e0782d07a7
Pranav Dhingra <pranav.dhingra@gmail.com>
2017-02-05 17:05:42 -0600

    Made functions.py file that contains all scraping functions to be used in app.py.

--- app.py
+++ app.py
@@ -91,15 +91,3 @@ def log(message):  # simple wrapper for logging to stdout on heroku
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-def getfirsttalk(page):
-    '''page is the html page from which we want to scrape.
-    Function gets the first talk from the page.'''
-    soup = BeautifulSoup(page.text, "html.parser")
-    talklinks = soup.find_all("a", href=re.compile('/talks/'))
-    parsedlinks = []
-    for o in talklinks:
-        parsedlinks.append(o['href'])
-    parsedlinks = list(set(parsedlinks))
-    return parsedlinks[0]
