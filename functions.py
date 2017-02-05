93e4fc686141e097af47133e32acbf29ed52ba27
Nikit Bobba <nikitbobba2019@u.northwestern.edu>
2017-02-05 17:14:51 -0600

    Added function to return url for video search

--- functions.py
+++ functions.py
@@ -14,3 +14,30 @@ def getfirsttalk(page):
         parsedlinks.append(o['href'])
     parsedlinks = list(set(parsedlinks))
     return parsedlinks[0]
+
+
+
+
+keyword = raw_input("Search Video: ")
+url = "https://www.ted.com/search?cat=talks&per_page=12&q="
+word_add = ""
+def SearchTedx(entry):
+	'''input will be a search for a video and returns the url for the list of all 
+	based on input'''
+	split_word = entry.split()
+	n = len(split_word)
+	word_add = split_word[0]
+
+	for i in range (1, int(n)):
+		word_add = word_add + "+"+split_word[i]
+
+	
+
+	print (url)
+	print (word_add)
+	print (url + word_add)
+	print split_word[0]
+	print n
+
+
+SearchTedx(keyword)
