5ee38216c3ca9e7bba4ff42c1f6448d06835b0fa
Avi Vaid <avaid1996@gmail.com>
2017-03-24 07:47:08 +0530

    putting userinfo into the main pipeline
    Signed-off-by: Avi Vaid <avaid1996@gmail.com>

--- app.py
+++ app.py
@@ -58,6 +58,10 @@ def webhook():
                     message_text = messaging_event["message"]["text"]  # the message's text
                     searchLink=SearchTedx(message_text)
                     vidLink=getFirstLink(searchLink)
+                    #get sender info
+                    userInfo = getUserInfo(str(recipient_id))
+                    log(userInfo)
+
                     send_message(sender_id, vidLink)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
